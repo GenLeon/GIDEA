70ec3512286523c446b276fd9abb6a9ed60f2155
GenLeon <86742263+GenLeon@users.noreply.github.com>
2021-12-07 18:15:12 +0300

    The lecture is over

---
+++ src/Lesson_01/JavaIntroductionClass.java
@@ -0,0 +1,103 @@
+package Lesson_01;
+
+public class JavaIntroductionClass {
+    public static void main(String[] args) {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+//
+//              int a = 10;
+//              int b = 11;
+//              int c = a + b;
+//              if (c >= 0) {
+//                     System.out.println("Сумма положительна");
+//              }
+//              if (c<0) {
+//                     System.out.println("Сумма отрицательна");
+//              }
+
+
+
+
+
+
+
+        //целые числа или числа без точки
+//              byte myByte = 16; //1 byte <-128..+127>
+//              short myShort = 316; //2 bytes <-32768..+32767>
+//              int myInteger = 99000; //4 bytes <-2.1ml-rd..+2.1ml-rd>
+//              long myLong = 30000000000L; //8 bytes <-2^63..+2^63>
+//
+//              //дробные числа или числа c точкой
+//              float myFloat = 316.66f; //4 bytes <>.00000000
+//              double myDouble = 31666835; //8 bytes <>.000000000000000 DEFAULT
+//
+//              //буквы или символы
+//              double myChar = 'q'; //2 bytes ~ASCII. UTF-16
+//
+//              //слово или набор символов или String
+//              String myString = "My String";
+//
+//              //логический тип данных или ТЫ МЕНЯ ОБМАНЫЫВАЕШЬ ? правда-ложь
+//              boolean myBooleanTrue = true;
+//              boolean myBooleanFalse = false;
+//
+//
+//              System.out.println (myFloat);
+//              System.out.println(myDouble);
+//              System.out.println(myBooleanFalse);
+//
+//              //Конкатенация строк
+//              System.out.println("My word is:" + myString + " > " + myLong);
+//              int abc = 100;
+//              if (abc > 150) {
+//                     System.out.println("Дубль 1:All Ok abc > 150");
+//              } else {
+//                     System.out.println("Дубль 1: No good abc < 150");
+//              }
+//              if (abc > 150) {
+//                     System.out.println("Дубль 2: All Ok abc > 150");
+//              }
+//              if (abc > 150) {
+//                     System.out.println("Дубль 3: All Ok abc > 150");
+//              } else if (abc == 100) {
+//                     System.out.println("Дубль 3: SUPER PRICE: abc == 100");
+//              } else {
+//                     System.out.println("Дубль 3: No good abc < 150");
+//              }
+        //Простейшие математические операции
+        //+ - * / ^
+
+//              double a = 15;
+//              float b = 35;
+//              System.out.println("Result = " + a / b);
+
+//              int value1 = 10;
+//              int value2 = 20;
+//              int result;
+
+//              result = value1 * value2 +2;
+//              System.out.println("Step 1: " + result);
+//              value1 = value1 +1;
+//              result = value1 * value2 +2;
+//              System.out.println("Step 2 :" + result);
+//              value2 = value2 + 1;
+//              result = value1 * value2 +2;
+//              System.out.println("Step 3:" + result);
+        //--------------------
+//              result = myCalculate(value1, value2);
+//              System.out.println("Step 1:" + result);
+//              value1 = value1 +1;
+//              result = myCalculate(value1, value2);
+//              System.out.println("Step 2:" + result);
+//              value2 = value2 +1;
+//              result = myCalculate(value1, value2);
+//              System.out.println("Step 3:" +  result);
+
+    }
+
+//       public static int myCalculate(int argInt1, int argInt2) {
+//              return argInt1 * argInt2 + 2;
+//       }
+
+}
